f40b00cb5225bc96f7a25c8b85f41310a9d52de7
Vitalii Martynovskyi <vitaliy.martynovskyy@ukr.net>
2021-07-25 14:21:12 +0300

    feat(tools): add safe handling for UpdateHandler

--- src/main/java/com/motokyi/tg/botapi/components/tools/UpdateHandler.java
+++ src/main/java/com/motokyi/tg/botapi/components/tools/UpdateHandler.java
@@ -1,15 +1,23 @@
 package com.motokyi.tg.botapi.components.tools;
 
-import com.motokyi.tg.botapi.api.types.*;
+import com.motokyi.tg.botapi.api.types.ChosenInlineResult;
+import com.motokyi.tg.botapi.api.types.InlineQuery;
+import com.motokyi.tg.botapi.api.types.Message;
+import com.motokyi.tg.botapi.api.types.PreCheckoutQuery;
+import com.motokyi.tg.botapi.api.types.ShippingQuery;
+import com.motokyi.tg.botapi.api.types.Update;
 import com.motokyi.tg.botapi.api.types.markup.CallbackQuery;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
 import static java.util.Objects.nonNull;
 import static java.util.Optional.ofNullable;
 
 @Builder
+@Slf4j
 public class UpdateHandler {
     private final Consumer<Message>
             messageConsumer,
@@ -24,15 +32,22 @@ public class UpdateHandler {
 
     public void apply(Update update) {
         if (nonNull(update)) {
-            ofNullable(update.getMessage()).ifPresent(this.messageConsumer);
-            ofNullable(update.getEditedMessage()).ifPresent(this.editedMessageConsumer);
-            ofNullable(update.getChannelPost()).ifPresent(this.channelPostConsumer);
-            ofNullable(update.getEditedMessage()).ifPresent(this.editedChannelPostConsumer);
-            ofNullable(update.getInlineQuery()).ifPresent(this.inlineQueryConsumer);
-            ofNullable(update.getChosenInlineResult()).ifPresent(this.chosenInlineResultConsumer);
-            ofNullable(update.getCallbackQuery()).ifPresent(this.callbackQueryConsumer);
-            ofNullable(update.getShippingQuery()).ifPresent(this.shippingQueryConsumer);
-            ofNullable(update.getPreCheckoutQuery()).ifPresent(this.preCheckoutQueryConsumer);
+            ofNullable(update.getMessage()).ifPresent(handleSafe(this.messageConsumer));
+            ofNullable(update.getEditedMessage()).ifPresent(handleSafe(this.editedMessageConsumer));
+            ofNullable(update.getChannelPost()).ifPresent(handleSafe(this.channelPostConsumer));
+            ofNullable(update.getEditedMessage()).ifPresent(handleSafe(this.editedChannelPostConsumer));
+            ofNullable(update.getInlineQuery()).ifPresent(handleSafe(this.inlineQueryConsumer));
+            ofNullable(update.getChosenInlineResult()).ifPresent(handleSafe(this.chosenInlineResultConsumer));
+            ofNullable(update.getCallbackQuery()).ifPresent(handleSafe(this.callbackQueryConsumer));
+            ofNullable(update.getShippingQuery()).ifPresent(handleSafe(this.shippingQueryConsumer));
+            ofNullable(update.getPreCheckoutQuery()).ifPresent(handleSafe(this.preCheckoutQueryConsumer));
         }
+    }
+
+    private <T> Consumer<T> handleSafe(Consumer<T> consumer) {
+        if (Objects.nonNull(consumer)) {
+            return consumer;
+        }
+        return t -> log.warn("Consumer for {} not defined", t.getClass().getSimpleName());
     }
 }
