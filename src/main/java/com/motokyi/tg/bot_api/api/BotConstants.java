74066eb8ee0f420e6d638e9608c443237e846acf
Vitalii Martynovskyi <vitalii@motokyi.com>
2023-01-07 22:18:50 +0200

    feat: add constants class

---
+++ src/main/java/com/motokyi/tg/bot_api/api/BotConstants.java
@@ -0,0 +1,9 @@
+package com.motokyi.tg.bot_api.api;
+
+public final class BotConstants {
+    public static final String MARKDOWN_V2 = "MarkdownV2";
+    public static final String HTML = "HTML";
+
+    private BotConstants() {
+    }
+}
