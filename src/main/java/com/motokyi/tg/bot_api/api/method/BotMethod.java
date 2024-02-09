9f29f851bc81d000dbf6af3939dff81a1a228019
Vitalii Martynovskyi <vitalii@motokyi.com>
2024-02-09 13:05:15 +0200

    feat(BotMethod): more subscribe method delegates

--- src/main/java/com/motokyi/tg/bot_api/api/method/BotMethod.java
+++ src/main/java/com/motokyi/tg/bot_api/api/method/BotMethod.java
@@ -1,14 +1,52 @@
 package com.motokyi.tg.bot_api.api.method;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
+import reactor.util.annotation.Nullable;
+import reactor.util.context.Context;
 
 import java.util.function.Consumer;
 
 public interface BotMethod<T> {
     Mono<T> send();
 
-    default Disposable subscribe(Consumer<T> consumer) {
+    default Disposable subscribe() {
+        return send().subscribe();
+    }
+
+    default Disposable subscribe(Consumer<? super T> consumer) {
         return send().subscribe(consumer);
     }
+
+    default void subscribe(Subscriber<? super T> actual) {
+        send().subscribe(actual);
+    }
+
+    default Disposable subscribe(@Nullable Consumer<? super T> consumer,
+                                 Consumer<? super Throwable> errorConsumer) {
+        return send().subscribe(consumer, errorConsumer);
+    }
+
+    default Disposable subscribe(@Nullable Consumer<? super T> consumer,
+                                 @Nullable Consumer<? super Throwable> errorConsumer,
+                                 @Nullable Runnable completeConsumer) {
+        return send().subscribe(consumer, errorConsumer, completeConsumer);
+    }
+
+    default Disposable subscribe(@Nullable Consumer<? super T> consumer,
+                                 @Nullable Consumer<? super Throwable> errorConsumer,
+                                 @Nullable Runnable completeConsumer,
+                                 @Nullable Consumer<? super Subscription> subscriptionConsumer) {
+        return send().subscribe(consumer, errorConsumer, completeConsumer, subscriptionConsumer);
+    }
+
+    default Disposable subscribe(@Nullable Consumer<? super T> consumer,
+                                 @Nullable Consumer<? super Throwable> errorConsumer,
+                                 @Nullable Runnable completeConsumer,
+                                 @Nullable Context initialContext) {
+        return send().subscribe(consumer, errorConsumer, completeConsumer, initialContext);
+    }
 }
+
